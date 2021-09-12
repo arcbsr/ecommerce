63a918b1aa757cf7d9b517ce5068ded5692a0e7e
Rafiur Rahman <bsrsoftbd@gmail.com>
2021-09-12 06:16:27 +0600

    commentout mongodb

--- routes/blogRoute.js
+++ routes/blogRoute.js
@@ -2,6 +2,6 @@ const express = require('express');
 const router = express.Router();
 const blogController = require('../controllers/blogController');
 
-router.get('/all',blogController.blog_index);
+router.get('/all',blogController.blog_list);
 router.get('/:id',blogController.blog_get);
 module.exports = router;
