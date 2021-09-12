f2921f72dc5d37b2279b88f59d9d2031c7a9e9d1
Rafiur Rahman <bsrsoftbd@gmail.com>
2021-09-12 06:10:55 +0600

    commentout mongodb

--- controllers/blogController.js
+++ controllers/blogController.js
@@ -1,32 +1,30 @@
 
-var MongoClient = require('mongodb').MongoClient;
+// var MongoClient = require('mongodb').MongoClient;
 
 //https://www.w3schools.com/nodejs/nodejs_mongodb_find.asp
 const dbURI = "mongodb+srv://ec2613:ec2613@appfinder.hclnu.mongodb.net/";
 const dbName = "ecommerce";
 const blog_index = (req, res) => {
-    
-
     const blog = [{ title: "this is title2" }];
     res.send(blog);
 }
-const blog_list = (req, res) => {
-    MongoClient.connect(dbURI, function (err, db) {
-        if (err) { console.log(err); return; }
-        var dbo = db.db(dbName);
-        var myobj = { jame: "Company Inc", address: "Highway 37" };
-        dbo.collection("users").find({}).toArray(function (err, result) {
-            if (err) { console.log(err); return; }
-            console.log(result);
-            //res.send(result);
+// const blog_list = (req, res) => {
+//     MongoClient.connect(dbURI, function (err, db) {
+//         if (err) { console.log(err); return; }
+//         var dbo = db.db(dbName);
+//         var myobj = { jame: "Company Inc", address: "Highway 37" };
+//         dbo.collection("users").find({}).toArray(function (err, result) {
+//             if (err) { console.log(err); return; }
+//             console.log(result);
+//             //res.send(result);
 
-            res.send(result);
-            db.close();
-        });
-    });
-    // const blog = [{ title: "this is title2" }];
-    // res.send(blog);
-}
+//             res.send(result);
+//             db.close();
+//         });
+//     });
+//     // const blog = [{ title: "this is title2" }];
+//     // res.send(blog);
+// }
 function User(name,age){
     this.name = name;
     this.age = age;
@@ -35,26 +33,26 @@ var fromJson = function (json){
     var obj = JSON.parse(JSON.stringify(json))
     return new User(obj.name, obj.age);
 };
-const blog_get = (req, res) => {
-    const datas = JSON.parse(JSON.stringify(req.query));
-    var user = fromJson(datas)
-    if(!user.name){
-        res.send("No name found")
-        return;
-    }
-    MongoClient.connect(dbURI, function (err, db) {
-        if (err) throw err;
-        var dbo = db.db(dbName);
-        //var myobj = { jame: "Company Inc", address: "Highway 37" };
+// const blog_get = (req, res) => {
+//     const datas = JSON.parse(JSON.stringify(req.query));
+//     var user = fromJson(datas)
+//     if(!user.name){
+//         res.send("No name found")
+//         return;
+//     }
+//     MongoClient.connect(dbURI, function (err, db) {
+//         if (err) throw err;
+//         var dbo = db.db(dbName);
+//         //var myobj = { jame: "Company Inc", address: "Highway 37" };
         
-        dbo.collection("users").insertOne(datas, function (err, result) {
-            if (err) throw err;
-            console.log("1 document inserted");
-            db.close();
-            res.send(datas);
-        });
-    });
-}
+//         dbo.collection("users").insertOne(datas, function (err, result) {
+//             if (err) throw err;
+//             console.log("1 document inserted");
+//             db.close();
+//             res.send(datas);
+//         });
+//     });
+// }
 
 
 
@@ -72,6 +70,6 @@ const blog_get = (req, res) => {
     //const user = new User(req.params);
 module.exports = {
     blog_index,
-    blog_get, blog_list
+    //blog_get, blog_list
 
 }
--- routes/blogRoute.js
+++ routes/blogRoute.js
@@ -2,7 +2,6 @@ const express = require('express');
 const router = express.Router();
 const blogController = require('../controllers/blogController');
 
-router.get('/all',blogController.blog_list);
-router.get('/:id',blogController.blog_get);
-router.post('/all',blogController.blog_list);
+router.get('/all',blogController.blog_index);
+//router.get('/:id',blogController.blog_get);
 module.exports = router;
