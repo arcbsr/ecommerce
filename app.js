6f4c5dfa4881d7f2e4c1f29f75313ea6a3027559
Rafiur Rahman <bsrsoftbd@gmail.com>
2021-09-12 06:56:22 +0600

    none

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
 const express = require('express');
 const morgan = require('morgan');
 const blogRouter = require('./routes/blogRoute');
+const mongoose = require('mongoose');
 
 const dbURI = "mongodb+srv://ec2613:ec2613@appfinder.hclnu.mongodb.net/ecommerce";
 
 const app = express();
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(result => app.listen(3000))
+  .catch(err => console.log(err));
 app.use(morgan('dev'));
 app.use(express.urlencoded({ extended: true }));
-app.listen(3000)
+//app.listen(3000)
 app.use(express.static('public'))
 app.get('/', (req, res) => {
 
--- controllers/blogController.js
+++ controllers/blogController.js
@@ -1,75 +1,51 @@
 
-// var MongoClient = require('mongodb').MongoClient;
+const Blog = require('./models/blog');
 
 //https://www.w3schools.com/nodejs/nodejs_mongodb_find.asp
 const dbURI = "mongodb+srv://ec2613:ec2613@appfinder.hclnu.mongodb.net/";
 const dbName = "ecommerce";
-  
+
 
 const blog_index = (req, res) => {
     res.send("I am here");
 }
 const blog_list = (req, res) => {
-    // MongoClient.connect(dbURI, function (err, db) {
-    //     if (err) { console.log(err); return; }
-    //     var dbo = db.db(dbName);
-    //     var myobj = { jame: "Company Inc", address: "Highway 37" };
-    //     dbo.collection("users").find({}).toArray(function (err, result) {
-    //         if (err) { console.log(err); return; }
-    //         console.log(result);
-    //         //res.send(result);
-
-    //         res.send(result);
-    //         db.close();
-    //     });
-    // });
-    const blog = [{ title: "this is title2" }];
-    res.send(blog);
+    Blog.find().sort({ createdAt: -1 })
+        .then(result => {
+            res.send(result);
+        })
+        .catch(err => {
+            console.log(err);
+        });
 }
-function User(name,age){
-    this.name = name;
-    this.age = age;
-};
-var fromJson = function (json){
-    var obj = JSON.parse(JSON.stringify(json))
-    return new User(obj.name, obj.age);
-};
+
 const blog_get = (req, res) => {
-    const datas = JSON.parse(JSON.stringify(req.query));
-    var user = fromJson(datas)
-    if(!user.name){
-        res.send("No name found")
-        return;
-    }
-    // MongoClient.connect(dbURI, function (err, db) {
-    //     if (err) throw err;
-    //     var dbo = db.db(dbName);
-    //     //var myobj = { jame: "Company Inc", address: "Highway 37" };
-        
-    //     dbo.collection("users").insertOne(datas, function (err, result) {
-    //         if (err) throw err;
-    //         console.log("1 document inserted");
-    //         db.close();
-    //         res.send(datas);
-    //     });
-    // });
-    res.send(req.params);
+    const data = {title:"bappy"}
+    const blog = new Blog(data);
+    blog.save()
+        .then(result => {
+            res.send(result);
+        })
+        .catch(err => {
+            console.log(err);
+        });
+    //res.send(req.params);
 }
 
 
 
 //res.send(req.params.id);
-    // console.log(process.env.NODE_ENV)
-    // const obj = JSON.parse(JSON.stringify(req.query));
-    // var length = Object.keys(obj).length;
-    // console.log(">>>"+length)
-    // if(length > 0){
-    //     //res.send(obj.count);
-    //     res.send("Name is " + obj.name);
-    // }
-    // else
-    //     res.send(obj);
-    //const user = new User(req.params);
+// console.log(process.env.NODE_ENV)
+// const obj = JSON.parse(JSON.stringify(req.query));
+// var length = Object.keys(obj).length;
+// console.log(">>>"+length)
+// if(length > 0){
+//     //res.send(obj.count);
+//     res.send("Name is " + obj.name);
+// }
+// else
+//     res.send(obj);
+//const user = new User(req.params);
 module.exports = {
     blog_index,
     blog_get, blog_list
